dc11ebb0fea52de6954cd759a4c460c83603ae01
Rishiraj Thadeshwar <rishiraj.thadeshwar@gmail.com>
2026-07-16 14:51:19 -0400

    Updates

--- Python/Rough.py
+++ Python/Rough.py
@@ -1,24 +1,16 @@
-import itertools
 class Solution:
-    def numberOfStableArrays(self, zero: int, one: int, limit: int) -> int:
-        MOD = (10**9) + 7
-        def dp(z,o,last,count):
-            if z == 0 and o == 0:
-                return 1
-            ans = 0
+    def countMajoritySubarrays(self, nums, target):
+        n=len(nums)
+        count=0
+        for i in range(n):
+            for j in range(i+1,n+1):
+                sub=nums[i:j]
+                #print(sub.count(target))
+                if sub.count(target)>len(sub)//2:
+                    count+=1
+        return count
 
-            if z > 0:
-                if last != 0:
-                    ans += dp(z-1, o, 0, 1)
-                elif count < limit:
-                    ans += dp(z-1, o, 0, count+1)
-
-            if o > 0:
-                if last != 1:
-                    ans += dp(z, o-1, 1, 1)
-                elif count < limit:
-                    ans += dp(z, o-1, 1, count+1)
-
-            return ans % MOD
 so=Solution()
-print(so.numberOfStableArrays(3,3,2))
+nums =[1,1,1,1]
+target =1
+print(so.countMajoritySubarrays(nums,target))
